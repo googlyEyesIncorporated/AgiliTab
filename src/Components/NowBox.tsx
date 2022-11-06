c493a2a95dd179c0a1bc2a3174f827a6af34612c
j-tirelli <john.tirelli@gmail.com>
2022-11-06 01:54:26 -0400

    Simplies weekday using luxon builtin

--- src/Components/NowBox.tsx
+++ src/Components/NowBox.tsx
@@ -2,17 +2,6 @@ import { DateTime } from "luxon";
 import { useEffect, useState } from "react";
 import { Clock } from "./currentTimeBox/Clock";
 
-const weekDays = [
-  "Unused",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursdays",
-  "Fridays",
-  "Saturday",
-  "Sunday",
-];
-
 export interface ClockProps {
   h24: boolean;
 }
@@ -38,7 +27,7 @@ export const NowBox = ({ h24 }: ClockProps) => {
     <div className="now" id="nowbox">
       <Clock date={date} h24={h24} />
       <div className="date" style={{ marginTop: "1rem" }}>
-        <div>{weekDays[date.weekday]}</div>
+        <div>{date.weekdayLong}</div>
         <div>{date.toFormat("MMM dd, yyyy")}</div>
       </div>
     </div>
