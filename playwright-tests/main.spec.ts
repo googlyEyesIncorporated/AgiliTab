30f9775845d66d57dfa3d4c3fc97a3bd617a5817
j-tirelli <john.tirelli@gmail.com>
2023-12-07 00:43:02 -0500

    Sets a consistent time for visual regression testing

--- playwright-tests/main.spec.ts
+++ playwright-tests/main.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
-
 const appPage = "http://127.0.0.1:3000";
+
+// Pick the new/fake "now" for you test pages.
+const fakeNow = new Date("September 25 2023 12:20:00").valueOf();
 
 test("has title", async ({ page }) => {
   await page.goto(appPage);
@@ -10,6 +12,23 @@ test("has title", async ({ page }) => {
 });
 
 test("passes visual regression", async ({ page }) => {
+  // Update the Date accordingly in your test pages
+  await page.addInitScript(`{
+    // Extend Date constructor to default to fakeNow
+    Date = class extends Date {
+      constructor(...args) {
+        if (args.length === 0) {
+          super(${fakeNow});
+        } else {
+          super(...args);
+        }
+      }
+    }
+    // Override Date.now() to start from fakeNow
+    const __DateNowOffset = ${fakeNow} - Date.now();
+    const __DateNow = Date.now;
+    Date.now = () => __DateNow() + __DateNowOffset;
+  }`);
   await page.goto(appPage);
   await expect(page).toHaveScreenshot();
 });
