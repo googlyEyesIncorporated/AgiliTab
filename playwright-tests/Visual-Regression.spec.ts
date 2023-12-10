12468257b3c05cea470767ae9a1c907e77c2478a
j-tirelli <john.tirelli@gmail.com>
2023-12-10 01:38:42 -0500

    Comments out most screen sizes from visual regression tests

--- playwright-tests/Visual-Regression.spec.ts
+++ playwright-tests/Visual-Regression.spec.ts
@@ -3,13 +3,13 @@ import { appPage, setTime } from "./utils";
 import { Locator } from "playwright-core";
 
 const viewports = [
+  // { width: 2560, height: 1400 },
   { width: 1920, height: 1080 },
-  { width: 1366, height: 768 },
-  { width: 1536, height: 864 },
-  { width: 1440, height: 900 },
+  // { width: 1600, height: 900 },
+  // { width: 1536, height: 864 },
+  // { width: 1440, height: 900 },
+  // { width: 1366, height: 768 },
   { width: 1280, height: 720 },
-  { width: 2560, height: 1400 },
-  { width: 1600, height: 900 },
 ];
 for (const viewport of viewports) {
   test.describe(`Visual Regression Testing (${viewport.width} x ${viewport.height})`, async () => {
