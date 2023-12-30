6e3ac7e47d0baef7dd6d6900b968fd45f3b392da
j-tirelli <john.tirelli@gmail.com>
2023-12-30 13:58:48 -0500

    moves InfoBox out of settings component

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -6,6 +6,7 @@ import { DateTimeFormat } from "./visual/DateTimeFormat";
 import { SetBooleanState } from "./term/WorkDay";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons/faCircleInfo";
 import Icon from "../atoms/Icon";
+import { InfoBox } from "./info";
 
 const handleClickOutside =
   (
@@ -19,8 +20,6 @@ const handleClickOutside =
       setIsInfoOpen(false);
     }
   };
-
-const agilitabDotCom = "http://agilitab.com"; // NOSONAR
 
 export const Settings = ({
   settingsContainer,
@@ -44,12 +43,10 @@ export const Settings = ({
         handleClickOutside(setHidden, setIsInfoOpen, settingsContainer)
       );
     };
-    // run on mount only
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps -- run on mount only
   }, []);
 
-  const { fontColor, bgColor, secondFontColor } =
-    useAppSelector(selectVisualSettings);
+  const { bgColor, secondFontColor } = useAppSelector(selectVisualSettings);
 
   return (
     <div
@@ -59,40 +56,7 @@ export const Settings = ({
       data-testid="hideable-settings"
       style={{ backgroundColor: bgColor }}
     >
-      <span
-        className={`popover z-10 h-fit absolute p-[5px] mx-0 mb-0 mt-[35px] left-0 right-0 top-0 bottom-0 border ${
-          isInfoOpen ? "" : " hidden"
-        }`}
-        style={{
-          borderColor: fontColor,
-          backgroundColor: bgColor,
-          color: fontColor,
-        }}
-      >
-        <ul>
-          <li className="grid" style={{ gridTemplateColumns: "1fr 4fr" }}>
-            <div>
-              <img src="/128.png" height={64} alt="logo" />
-            </div>
-            <div className="mt-2.5">
-              <ul>
-                <li>
-                  AgiliTab - <a href={agilitabDotCom}>agilitab.com</a>
-                </li>
-                <li>Developer: John Tirelli</li>
-              </ul>
-            </div>
-          </li>
-          <li>Special thanks:</li>
-          <li>
-            <a href="https://chrome.google.com/webstore/detail/prioritab/napbejkndjhcciibiglkimmgdlfjcbnp">
-              - Prioritab
-            </a>{" "}
-            - The primary inspiration of the design of this extension.
-          </li>
-          <li>- Zeah D. - Design Consultant</li>
-        </ul>
-      </span>
+      {isInfoOpen && <InfoBox />}
       <h1 className="font-bold text-2xl leading-none" id="settings-title">
         Customization
       </h1>
