d33a30cdfb41034f6c11fca3167f00c63699dda6
j-tirelli <john.tirelli@gmail.com>
2024-01-10 01:28:58 -0500

    Turns updateStorage test back on

--- src/features/utils/__tests__/storageHelpers.spec.ts
+++ src/features/utils/__tests__/storageHelpers.spec.ts
@@ -1,6 +1,6 @@
 import { initialSettings } from "../../settings/settingsSlice";
-// import { updateStorage, getStorage } from "../storageHelpers";
 import { getStorage } from "../storageHelpers";
+import { updateStorage } from "../updateStorage";
 
 const syncSet = jest.fn();
 const syncGet = jest.fn(
@@ -44,11 +44,11 @@ global.chrome = {
 
 describe("updateStorage", () => {
   it("calls chrome.storage.sync.set with stuff", () => {
-    // updateStorage({
-    //   storageKey: "settings",
-    //   val: initialSettings,
-    // });
-    // expect(syncSet).toHaveBeenCalledWith({ settings: undefined });
+    updateStorage({
+      storageKey: "settings",
+      val: initialSettings,
+    });
+    expect(syncSet).toHaveBeenCalledWith({ settings: undefined });
   });
 });
 
