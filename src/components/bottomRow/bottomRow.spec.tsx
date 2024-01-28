821345492936224c79de0e9630091dff1a4c6bf1
j-tirelli <john.tirelli@gmail.com>
2024-01-28 02:56:46 -0500

    update snapshots to be more reliable

--- src/components/bottomRow/bottomRow.spec.tsx
+++ src/components/bottomRow/bottomRow.spec.tsx
@@ -3,8 +3,15 @@ import { BottomRow } from ".";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
 import "@testing-library/jest-dom";
+import { epochTimes } from "../../commonTestData.json";
+
+const { Sep012023 } = epochTimes;
+const jestSetTime = (newDateTime: number) => {
+  jest.useFakeTimers().setSystemTime(new Date(newDateTime));
+};
 
 describe("bottomRow", () => {
+  jestSetTime(Sep012023);
   it("should display 3 lists, shortTerm, mediumTerm, longTerm", () => {
     render(
       <Provider store={store}>
