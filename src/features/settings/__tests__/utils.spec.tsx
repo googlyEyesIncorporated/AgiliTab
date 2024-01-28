85c4b430ca1ca8c56278603aa25acb41795d073f
j-tirelli <john.tirelli@gmail.com>
2024-01-28 04:50:19 -0500

    Adds saveTerm UTs

--- src/features/settings/__tests__/utils.spec.tsx
+++ src/features/settings/__tests__/utils.spec.tsx
@@ -1,1 +1,64 @@
-describe("saveTerm", () => {});
+import { DurationState } from "../types";
+import { saveTerm } from "../utils";
+
+const mockDispatch = jest.fn();
+
+const commonParams = {
+  title: "test",
+  unitType: "Duration Type",
+  startDate: "startDate",
+  groupId: 1,
+  endDate: "endDate",
+};
+
+const durationParams = {
+  ...commonParams,
+  isDuration: true,
+  duration: { unit: "days", qty: 1 },
+};
+
+const endDateParams = {
+  ...commonParams,
+  isDuration: false,
+  duration: {} as DurationState,
+};
+
+const expectedDurationDispatch = {
+  payload: {
+    key: 1,
+    termObj: {
+      duration: durationParams.duration,
+      isDuration: durationParams.isDuration,
+      startDate: durationParams.startDate,
+      title: durationParams.title,
+      unitType: durationParams.unitType,
+    },
+  },
+  type: "units/setNotShortTerm",
+};
+
+const expectedEndDateDispatch = {
+  payload: {
+    key: 1,
+    termObj: {
+      isDuration: endDateParams.isDuration,
+      startDate: endDateParams.startDate,
+      endDate: endDateParams.endDate,
+      title: endDateParams.title,
+      unitType: endDateParams.unitType,
+    },
+  },
+  type: "units/setNotShortTerm",
+};
+
+describe("saveTerm", () => {
+  it("should dispatch a unit setting action with duration", () => {
+    saveTerm({ enabled: true, dispatch: mockDispatch, ...durationParams });
+    expect(mockDispatch).toHaveBeenCalledWith(expectedDurationDispatch);
+  });
+
+  it("should dispatch a unit setting action with end date", () => {
+    saveTerm({ enabled: true, dispatch: mockDispatch, ...endDateParams });
+    expect(mockDispatch).toHaveBeenCalledWith(expectedEndDateDispatch);
+  });
+});
