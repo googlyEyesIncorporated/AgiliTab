a68ce5b47624348869ae31d8d559404c99ae0402
j-tirelli <john.tirelli@gmail.com>
2024-01-28 00:25:17 -0500

    remove advanceTerm

--- to_remove/advanceTerm.ts
+++
@@ -1,7 +0,0 @@
-import { StartEndUnitType } from "../src/features/itemList/types";
-
-export const advanceTermGen = (termObject: StartEndUnitType) => {
-  const duration = termObject.end - termObject.start;
-  termObject.start = termObject.end + 1;
-  termObject.end = termObject.start + duration;
-};
