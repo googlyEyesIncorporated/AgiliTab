ecabcacaebad275a9257383f574dffbf173f88b1
j-tirelli <john.tirelli@gmail.com>
2024-02-07 03:03:39 -0500

    Adds TermName tests

---
+++ src/components/settings/term/TermName.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { TermName } from "./TermName";
+import { ComponentProps } from "react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+
+const props: ComponentProps<typeof TermName> = {
+  title: "Test",
+  groupId: 0,
+  onChange: jest.fn(),
+};
+
+describe("TermName", () => {
+  const user = userEvent.setup();
+
+  it("should render a Term Name Settings dialogue", () => {
+    render(<TermName {...props} />);
+    expect(screen.getByText("Name:")).toBeInTheDocument();
+  });
+  it("should send back a title after updating it in the interface", async () => {
+    render(<TermName {...props} />);
+    const newUserName = "Updated";
+    await user.dblClick(screen.getByTestId("group-0-unit-name"));
+    await user.paste(newUserName);
+    expect(props.onChange).toHaveBeenCalledWith({
+      title: newUserName,
+    });
+  });
+});
