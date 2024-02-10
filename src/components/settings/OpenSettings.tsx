cbb3ea9fd18769181057386a53eaf1f8cced4da9
j-tirelli <john.tirelli@gmail.com>
2024-02-10 16:14:08 -0500

    Adds button for addTerm

--- src/components/settings/OpenSettings.tsx
+++ src/components/settings/OpenSettings.tsx
@@ -1,25 +1,36 @@
 import { useRef, useState } from "react";
 import { faGears } from "@fortawesome/free-solid-svg-icons/faGears";
+import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
 import { Settings } from ".";
 import Icon from "../atoms/Icon";
+import { useAppDispatch } from "../../app/hooks";
+// import { addTerm } from "../../features/itemList/itemListSlicev2";
 
 export const SettingsWrapper = () => {
   const [hidden, setHidden] = useState(true);
   const settingsContainer = useRef(null as HTMLDivElement | null);
+  const dispatch = useAppDispatch();
 
   return (
-    <div
-      data-testid="settings"
-      className="fixed left-0 bottom-0 m-4"
-      ref={settingsContainer}
-    >
-      <Icon
-        onClick={() => setHidden(!hidden)}
-        icon={faGears}
-        faClassName={`fade-in-1s text-double cursor-pointer${
-          !hidden ? " hidden" : ""
-        }`}
-      />
+    <div className="fixed left-0 bottom-0 m-4" ref={settingsContainer}>
+      <div data-testid="add-group" className="text-center mb-2">
+        <Icon
+          // onClick={() => dispatch(addTerm())}
+          icon={faPlus}
+          faClassName={`fade-in-1s text-double cursor-pointer${
+            !hidden ? " hidden" : ""
+          }`}
+        />
+      </div>
+      <div data-testid="settings">
+        <Icon
+          onClick={() => setHidden(!hidden)}
+          icon={faGears}
+          faClassName={`fade-in-1s text-double cursor-pointer${
+            !hidden ? " hidden" : ""
+          }`}
+        />
+      </div>
       <Settings
         settingsContainer={settingsContainer}
         hideSettings={hidden}
