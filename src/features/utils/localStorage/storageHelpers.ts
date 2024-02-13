250d1de5149b30ad64f3e014655a95e6588d0b36
j-tirelli <john.tirelli@gmail.com>
2024-02-12 23:13:29 -0500

    simplifies storage helpers

--- src/features/utils/localStorage/storageHelpers.ts
+++ src/features/utils/localStorage/storageHelpers.ts
@@ -6,30 +6,27 @@ import { populateTasksFromChrome } from "../../itemList/itemListSlice";
 export const getStorage = (dispatch: ReturnType<typeof useAppDispatch>) => {
   if (typeof chrome !== "undefined" && chrome.storage) {
     // chrome.storage.sync.clear();
-    chrome.storage.sync.get(
-      ["shortTermList", "mediumTermList", "longTermList", "settings"],
-      function (result) {
-        const shortTermList = result["shortTermList"]
-          ? (Object.values(result["shortTermList"]) as ItemList)
-          : [];
-        const mediumTermList = result["mediumTermList"]
-          ? (Object.values(result["mediumTermList"]) as ItemList)
-          : [];
-        const longTermList = result["longTermList"]
-          ? (Object.values(result["longTermList"]) as ItemList)
-          : [];
-        const settings = result["settings"]
-          ? (result["settings"] as string)
-          : "{}";
-        dispatch(populateSettingssFromChrome(JSON.parse(settings)));
-        dispatch(
-          populateTasksFromChrome({
-            shortTermList,
-            mediumTermList,
-            longTermList,
-          })
-        );
-      }
-    );
+    chrome.storage.sync.get(null, function (result) {
+      const shortTermList = result["shortTermList"]
+        ? (Object.values(result["shortTermList"]) as ItemList)
+        : [];
+      const mediumTermList = result["mediumTermList"]
+        ? (Object.values(result["mediumTermList"]) as ItemList)
+        : [];
+      const longTermList = result["longTermList"]
+        ? (Object.values(result["longTermList"]) as ItemList)
+        : [];
+      const settings = result["settings"]
+        ? (result["settings"] as string)
+        : "{}";
+      dispatch(populateSettingssFromChrome(JSON.parse(settings)));
+      dispatch(
+        populateTasksFromChrome({
+          shortTermList,
+          mediumTermList,
+          longTermList,
+        })
+      );
+    });
   }
 };
