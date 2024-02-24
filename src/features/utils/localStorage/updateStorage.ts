da021e4f3522cb963bccf9281bde6cc54576dd61
j-tirelli <john.tirelli@gmail.com>
2024-02-24 15:44:33 -0500

    technically not untru

--- src/features/utils/localStorage/updateStorage.ts
+++ src/features/utils/localStorage/updateStorage.ts
@@ -1,11 +1,9 @@
-import { ListTypes } from "../../itemList/types";
-
 export const updateStorage = ({
   storageKey,
   val,
 }: {
   storageKey: string;
-  val: ListTypes | string;
+  val: any;
 }) => {
   if (typeof chrome !== "undefined" && chrome.storage) {
     chrome.storage.sync.set({ [storageKey]: val });
