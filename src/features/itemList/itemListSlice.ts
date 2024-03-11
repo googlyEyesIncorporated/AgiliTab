20416a53658816c7affcd7d167ca9bdb91026539
j-tirelli <john.tirelli@gmail.com>
2024-03-10 21:02:57 -0400

    updateList

--- src/features/itemList/itemListSlice.ts
+++ src/features/itemList/itemListSlice.ts
@@ -14,8 +14,6 @@ import {
 import { updateStorage } from "../utils/localStorage/updateStorage";
 import { RootState } from "../../app/commonTypes";
 
-// TODO: start off better
-
 const initialTodoListState: ItemListState = {
   itemList: {},
   deleteHistory: [],
@@ -30,91 +28,127 @@ export const itemListSlice = createSlice({
   reducers: {
     add: (state, action: PayloadAction<ListAndItem>) => {
       const { listKey, item } = action.payload;
-      state.itemList[listKey].list.push(item);
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      const list = [...state.itemList[listKey].list, item];
+      state.itemList[listKey].list = list;
+      updateStorage({
+        storageKey: listKey,
+        val: { ...state.itemList[listKey], list },
+      });
     },
     addTerm: (state, action: PayloadAction<CreateList>) => {
       const { listKey, listObject } = action.payload;
-      state.listOrder.push(listKey);
+      const listOrder = [...state.listOrder, listKey];
+      state.listOrder = listOrder;
       state.itemList[listKey] = listObject;
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
-      updateStorage({ storageKey: "listOrder", val: state.listOrder });
+      updateStorage({ storageKey: listKey, val: listObject });
+      updateStorage({ storageKey: "listOrder", val: listOrder });
     },
     remove: (state, action: PayloadAction<ListAndIndex>) => {
       const { listKey, index } = action.payload;
-      state.deleteHistory.push({
-        items: [state.itemList[listKey].list[index]],
-        listKey,
-      });
+      const deleteHistory = [
+        ...state.deleteHistory,
+        {
+          items: [state.itemList[listKey].list[index]],
+          listKey,
+        },
+      ];
+      state.deleteHistory = deleteHistory;
+      const list = [...state.itemList[listKey].list];
+      list.splice(index, 1);
+      const itemList = { ...state.itemList[listKey], list };
       state.shouldShowToaster = true;
-      state.itemList[listKey].list.splice(index, 1);
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      state.itemList[listKey] = itemList;
+      updateStorage({ storageKey: listKey, val: itemList });
     },
     removeTerm: (state, action: PayloadAction<JustListKey>) => {
       const { listKey } = action.payload;
-      state.deleteHistory.push({
-        list: state.itemList[listKey],
-        listKey,
-      });
+      const deleteHistory = [
+        ...state.deleteHistory,
+        {
+          list: state.itemList[listKey],
+          listKey,
+        },
+      ];
       const positionInOrder = state.listOrder.findIndex(
         (value) => value === listKey
       );
       if (positionInOrder !== -1) {
-        state.listOrder.splice(positionInOrder, 1);
+        const listOrder = [...state.listOrder];
+        listOrder.splice(positionInOrder, 1);
+
+        state.listOrder = listOrder;
+        updateStorage({ storageKey: "listOrder", val: listOrder });
       }
       const { [listKey]: termToRemove, ...everythingElse } = state.itemList;
+      state.deleteHistory = deleteHistory;
+      state.shouldShowToaster = true;
       state.itemList = everythingElse;
       updateStorage({ storageKey: listKey, val: null });
-      updateStorage({ storageKey: "listOrder", val: state.listOrder });
     },
     moveTerm: (state, action: PayloadAction<{ listOrder: string[] }>) => {
       state.listOrder = action.payload.listOrder;
-      updateStorage({ storageKey: "listOrder", val: state.listOrder });
+      updateStorage({ storageKey: "listOrder", val: action.payload.listOrder });
     },
     toggleChecked: (state, action: PayloadAction<ListAndIndex>) => {
       const { listKey, index } = action.payload;
-      state.itemList[listKey].list[index].done =
-        !state.itemList[listKey].list[index].done;
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      const list = [...state.itemList[listKey].list];
+      list[index].done = !list[index].done;
+      const itemList = { ...state.itemList[listKey], list };
+      state.itemList[listKey] = itemList;
+      updateStorage({ storageKey: listKey, val: itemList });
     },
     clearDone: (state, action: PayloadAction<JustListKey>) => {
       const { listKey } = action.payload;
       const deleteList: ItemList = [];
-      state.itemList[listKey].list = state.itemList[listKey].list.filter(
-        (item) => {
-          if (item.done) {
-            deleteList.push(item);
-          }
-          return !item.done;
+      const list = state.itemList[listKey].list.filter((item) => {
+        if (item.done) {
+          deleteList.push(item);
+          return false;
         }
-      );
-      state.deleteHistory.push({ items: deleteList, listKey });
+        return true;
+      });
+      const itemList = { ...state.itemList[listKey], list };
+      state.itemList[listKey] = itemList;
+      const deleteHistory = [
+        ...state.deleteHistory,
+        { items: deleteList, listKey },
+      ];
+      state.deleteHistory = deleteHistory;
       state.shouldShowToaster = true;
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      updateStorage({ storageKey: listKey, val: itemList });
     },
     clearAll: (state, action: PayloadAction<JustListKey>) => {
       const { listKey } = action.payload;
-      state.deleteHistory.push({
-        items: state.itemList[listKey].list,
-        listKey,
-      });
+      const deleteHistory = [
+        ...state.deleteHistory,
+        {
+          items: state.itemList[listKey].list,
+          listKey,
+        },
+      ];
+      state.deleteHistory = deleteHistory;
       state.shouldShowToaster = true;
-      state.itemList[listKey].list = [];
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      const itemList = { ...state.itemList[listKey], list: [] };
+      state.itemList[listKey] = itemList;
+      updateStorage({ storageKey: listKey, val: itemList });
     },
     updateList: (state, action: PayloadAction<ReplaceList>) => {
-      const { listKey, itemList, save = true } = action.payload;
-      state.itemList[listKey].list = itemList;
-      save &&
-        updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      const { listKey, itemList: list, save = true } = action.payload;
+      const itemList = { ...state.itemList[listKey], list };
+      state.itemList[listKey] = itemList;
+      save && updateStorage({ storageKey: listKey, val: itemList });
     },
     updateListItem: (
       state,
       action: PayloadAction<JustListKey & { index: number; name: string }>
     ) => {
       const { listKey, name, index } = action.payload;
-      state.itemList[listKey].list[index].name = name;
-      updateStorage({ storageKey: listKey, val: state.itemList[listKey] });
+      const list = [...state.itemList[listKey].list];
+      list[index].name = name;
+      const itemList = { ...state.itemList[listKey], list };
+
+      state.itemList[listKey] = itemList;
+      updateStorage({ storageKey: listKey, val: itemList });
     },
     populateTasksFromChrome: (
       state,
@@ -122,25 +156,38 @@ export const itemListSlice = createSlice({
     ) => {
       state.itemList = { ...state.itemList, ...itemList };
     },
+    populateTaskOrderFromChrome: (
+      state,
+      { payload: { listOrder } }: PayloadAction<{ listOrder: string[] }>
+    ) => {
+      state.listOrder = listOrder;
+    },
     undoDelete: (state) => {
       if (state.deleteHistory.length) {
         const lastIndex = state.deleteHistory.length - 1;
-        const { listKey, items, list } = state.deleteHistory[lastIndex];
+        const {
+          listKey,
+          items,
+          list: deletedItemList,
+        } = state.deleteHistory[lastIndex];
+        const itemList = state.itemList[listKey];
         state.deleteHistory = state.deleteHistory.slice(0, lastIndex);
-        if (listKey || list) {
-          if (list) {
-            state.itemList[listKey] = list;
+
+        if (listKey) {
+          if (deletedItemList) {
+            state.itemList[listKey] = deletedItemList;
             state.listOrder.push(listKey);
-            updateStorage({ storageKey: listKey, val: list });
-            updateStorage({ storageKey: "listOrder", val: state.listOrder });
+            updateStorage({ storageKey: listKey, val: deletedItemList });
+            updateStorage({
+              storageKey: "listOrder",
+              val: [...state.listOrder],
+            });
           } else if (Array.isArray(items)) {
-            state.itemList[listKey].list = [
-              ...state.itemList[listKey].list,
-              ...items,
-            ];
+            itemList.list = [...itemList.list, ...items];
+            state.itemList[listKey] = itemList;
             updateStorage({
               storageKey: listKey,
-              val: state.itemList[listKey],
+              val: itemList,
             });
           }
         }
@@ -158,14 +205,13 @@ export const itemListSlice = createSlice({
         listKey: string;
       }>
     ) => {
-      state.itemList[listKey] = { ...state.itemList[listKey], ...termPart };
-      updateStorage({
-        storageKey: listKey,
-        val: {
-          ...state.itemList[listKey],
-          list: [...state.itemList[listKey].list],
-        },
-      });
+      const itemList = {
+        ...state.itemList[listKey],
+        list: [...state.itemList[listKey].list],
+        ...termPart,
+      };
+      state.itemList[listKey] = itemList;
+      updateStorage({ storageKey: listKey, val: itemList });
     },
   },
 });
@@ -183,6 +229,7 @@ export const {
   updateList,
   updateListItem,
   populateTasksFromChrome,
+  populateTaskOrderFromChrome,
   toggleShouldShowToaster,
   setPartialTerm,
 } = itemListSlice.actions;
